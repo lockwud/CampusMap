4d54d9f0779cc61efa47681b69109656e8912df0
lockwud <oklement3@gmail.com>
2025-02-25 00:55:04 +0000

    BUG: Controllers for navigation not found. FIX: importing the navigation module in the app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,17 +5,17 @@ import { BuildingsModule } from './buildings/buildings.module';
 import { GoogleMapsService } from './google-maps/google-maps.service';
 import { PathFinderModule } from './path-finder/path-finder.module';
 import { GoogleMapsController } from './google-maps/google-maps.controller';
-import { LocationGateway } from './navigation/navigation.gateway';
 import { RoomsModule } from './rooms/rooms.module';
 import { EntrancesModule } from './entrances/entrances.module';
 import { PrismaService } from '../prisma/prisma.service';
 import { PathfindingService } from './path-finder/path-finder.service';
 import { FloorsModule } from './floors/floors.module';
 import { RoomNeighborModule } from './room-neighbor/room-neighbor.module';
+import { LocationModule } from "./navigation/navigation.module"
 
 @Module({
-  imports: [BuildingsModule, PathFinderModule, RoomsModule, EntrancesModule, FloorsModule, RoomNeighborModule],
+  imports: [BuildingsModule, PathFinderModule, RoomsModule, EntrancesModule, FloorsModule, RoomNeighborModule, LocationModule],
   controllers: [AppController, GoogleMapsController],
-  providers: [AppService, GoogleMapsService, LocationGateway, PrismaService, PathfindingService],
+  providers: [AppService, GoogleMapsService, PrismaService, PathfindingService],
 })
 export class AppModule {}
--- backend/src/navigation/navigation.module.ts
+++ backend/src/navigation/navigation.module.ts
@@ -3,8 +3,10 @@ import { LocationGateway } from './navigation.gateway';
 import { PrismaService } from '../../prisma/prisma.service';
 import { GoogleMapsService } from '../google-maps/google-maps.service';
 import { PathfindingService } from '../path-finder/path-finder.service';
+import { NavigationController } from './navigation.controller';
 
 @Module({
   providers: [LocationGateway, PrismaService, GoogleMapsService, PathfindingService],
+  controllers: [NavigationController]
 })
 export class LocationModule {}
